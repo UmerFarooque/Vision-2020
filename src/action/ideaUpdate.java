0427bed8dccfe34eb29f6aca7f45763af2c3377c
Mohammed Umer Farooque <51690713+UmerFarooque@users.noreply.github.com>
2019-08-01 16:51:34 +0530

    Update ideaUpdate.java

--- src/action/ideaUpdate.java
+++ src/action/ideaUpdate.java
@@ -1,1 +1,41 @@
+package action;
 
+import dao.IdeaDAO;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+public class ideaUpdate extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String status = request.getParameter("status");
+        if(!status.equals("") && !request.getParameter("ideaId").equals("")) {
+            try {
+                int id = Integer.parseInt(request.getParameter("ideaId"));
+                boolean done = IdeaDAO.updateStatus(id, status);
+                if(done)
+                    request.setAttribute("message", "Updated Successfully!!!");
+                request.getRequestDispatcher("display.jsp").forward(request, response);
+            } catch(SQLException e) { }
+            catch(ClassNotFoundException e) { }
+    	} else {
+			request.setAttribute("error","Permission denied");
+			RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
+			rd.forward(request, response);
+    	}
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+    
+}

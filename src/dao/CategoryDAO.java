c1548e7e83f586ac629e0f1f4ec87e52615b171a
Mohammed Umer Farooque <51690713+UmerFarooque@users.noreply.github.com>
2019-08-01 16:57:05 +0530

    Update CategoryDAO.java

--- src/dao/CategoryDAO.java
+++ src/dao/CategoryDAO.java
@@ -1,1 +1,25 @@
+package dao;
 
+import domain.Category;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class CategoryDAO {
+    public static List<Category> getAllCategory() throws SQLException, ClassNotFoundException {
+        
+        List<Category> categories = new ArrayList<Category>();
+        Connection con = DBConnection.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM category");
+        while(rs.next()) {
+            Category cat = new Category(rs.getInt(1), rs.getString(2));
+            categories.add(cat);
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return categories;
+        
+    }
+}
